38b6d380529db4a6a8bf9e27f3281681ba95defa
JakedeVos <jakedevos4@gmail.com>
2024-11-12 14:07:36 -0500

    Added Timer and Spawner For Food

---
+++ unity-project/mini-game-collection/Assets/2024/Team02/Scripts/Timer.cs
@@ -0,0 +1,25 @@
+using JetBrains.Annotations;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace MiniGameCollection.Games2024.Team02
+{
+    public class Timer : MonoBehaviour
+    {
+        [SerializeField] TextMeshProUGUI timerText;
+        [SerializeField] float remainingTime;
+
+        void Update()
+        {
+            remainingTime -= Time.deltaTime;
+            int minutes = Mathf.FloorToInt(remainingTime / 60);
+            int seconds = Mathf.FloorToInt(remainingTime % 60);
+            timerText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
+        }
+
+    }
+}
+
